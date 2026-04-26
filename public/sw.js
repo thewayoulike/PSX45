f50ee09e509394d10c0fe0272b6a145899964ec9
thewayoulike <itruth2011@gmail.com>
2026-04-26 00:26:19 -0400

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('push', function(event) {
+  if (event.data) {
+    const data = event.data.json();
+    
+    const options = {
+      body: data.body,
+      icon: '/pwa-192x192.png',
+      badge: '/pwa-64x64.png',
+      vibrate: [200, 100, 200]
+    };
+
+    event.waitUntil(
+      self.registration.showNotification(data.title, options)
+    );
+  }
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('/')
+  );
+});
