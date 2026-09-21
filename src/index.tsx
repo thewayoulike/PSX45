a6e57b88f9e757d2e3d80bdf4de18ca8b5728a94
thewayoulike <itruth2011@gmail.com>
2026-09-21 17:33:04 -0400

    a

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/Entry';
 import './entry.css';
-import './visual-theme.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
